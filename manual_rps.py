e9e2f3183214b9dd0f16f1063950d0ca692d9ae0
Anosh Malik <anoshmalik@Anoshs-MacBook-Pro.local>
2023-09-09 03:08:37 +0100

    manual_rps.py code now in a more structured format

--- manual_rps.py
+++ manual_rps.py
@@ -1,39 +1,41 @@
 import random 
 
-#def get_computer_choice():
-    #rps_list = ["rock", "paper", "scissors"]
-    #computer_choice = random.choice(rps_list)
-    #return computer_choice
-
-#def get_user_choice():
-    #user_choice = input("Choose rock, paper or scissors: ")
-    #if user_choice.lower() == "rock" or "paper" or "scissors":
-        #return user_choice
-    #else:
-       # print("Invalid input, make sure you choose rock, paper or scissors")
-        
-
-def play():
-
+def get_computer_choice():
     rps_list = ["rock", "paper", "scissors"]
     computer_choice = random.choice(rps_list)
-    
+    return computer_choice
+
+def get_user_choice():
     user_choice = input("Choose rock, paper or scissors: ")
+    return user_choice
+    
+        
+
+def get_winner(user_choice, computer_choice):
 
 
     
     if computer_choice.lower() == user_choice.lower():
         print("It's a tie")
-    elif computer_choice.lower() == "rock" and user_choice.lower() == "scissors":
-        print("You lost!")
-    elif computer_choice.lower() == "paper" and user_choice.lower() == "rock":
-        print("You lost!") 
-    elif computer_choice.lower() == "scissors" and user_choice.lower() == "paper":
-        print("You lost!")
-    elif user_choice.lower() not in rps_list:
-        print("Invalid input, please enter either rock, paper or scissors: ")
+    elif (
+         (computer_choice.lower() == "rock" and user_choice.lower() == "scissors") or
+         (computer_choice.lower() == "paper" and user_choice.lower() == "rock") or
+         (computer_choice.lower() == "scissors" and user_choice.lower() == "paper")
+    ):
+                                                                            
+        return "You lost!"
+    else:
+        return "You won!"
+        
+def play():
+    computer_choice = get_computer_choice()
+    user_choice = get_user_choice()
+    if user_choice.lower() not in ["rock", "paper", "scissors"]:
+        print("Invalid input, please enter either rock, paper or scissors.")
         play()
     else:
-        print("You won!")
-        
+        result = get_winner(user_choice, computer_choice)
+        return result
+    
+
 play()
